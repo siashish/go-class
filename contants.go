0df83fa9ccd72e962ca00c02daebb923d26d8cca
Ashish Singh <ashishsingh21798@gmail.com>
2023-08-29 20:05:52 +0530

    loop

---
+++ contants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	const World = "Chandrayaan 3"
+	fmt.Println("Hello ", World)
+	fmt.Println("Happy ", Pi, " Day")
+
+	const Truth = true
+	fmt.Println("Go rules? ", Truth)
+	fmt.Printf("%T", Truth)
+}
---
+++ type-inference.go
@@ -0,0 +1,15 @@
+package main
+
+// var i int
+// j := i
+
+// i := 42
+// f := 3.14
+// g := 0.867 + 0.5i
+
+import "fmt"
+
+func main() {
+	v := false
+	fmt.Printf("v is of type %T\n", v)
+}
